d9676eecba24399d78572aedf38cbee5cddc26cc
sittangwesa <sitangwesa6gmail.com>
2024-01-04 21:09:57 +0300

    login and logout added

--- base/views.py
+++ base/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from . forms import CreateUserForm, LoginForm
 from django.contrib.auth.models import auth
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
 def home(request):
     return render (request, 'pages/index.html')
@@ -21,7 +22,28 @@ def register(request):
     return render(request, 'pages/register.html', context = context)
 
 def login(request):
-    return render(request, 'pages/login.html')
+    form = LoginForm()
+    
+    if request.method == 'POST':
+        form = LoginForm(request, data = request.POST )
+        if form.is_valid():
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+            
+            user = authenticate(request, username=username, password= password)
+            
+            if user is not None:
+                auth.login(request, user)
+                return redirect ("userdashboard")
+                
+    context = {'form': form}
+    return render(request, 'pages/login.html', context)
 
 def logout(request):
+    
+    auth.logout(request)
     return render(request, 'pages/index.html')
+
+@login_required(login_url="login")
+def userDashboard(request):
+    return render(request, 'pages/userdashboard.html')
